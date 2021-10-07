f3d069f85fa091033b4eb5f623663434803eb366
daniyal-mck <daniyal.mackeyabadi@divar.ir>
2021-10-07 14:43:52 +0330

    t_error added

--- src/lexer.py
+++ src/lexer.py
@@ -22,6 +22,10 @@ t_ignore_COMMENT = r'\/\/.*'
 t_ignore_MULTICOMEMNT = r'\/\*((?!\*\/)(.|\n))*\*\/'
 t_ignore  = ' \t'
 
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
