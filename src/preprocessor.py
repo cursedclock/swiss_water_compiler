31928217cc08dcc6df77fb78942f4c256e564bb0
mostafa <mostafa@pop-os.localdomain>
2021-10-15 19:16:37 +0330

    added *= operator and fixed bugs with preprocessor

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,23 +1,22 @@
 import re
 from collections import OrderedDict
 
-_id_pattern = r'(^|\s|[^a-zA-Z0-9])({})(\s|$|[^a-zA-z0-9])'
+_id_pattern = r'(?<![a-zA-Z0-9_]){}(?![a-zA-z0-9_])'
 
 def pre_process(text: str) -> str:
     definitions = OrderedDict()
     lines = text.splitlines()
     processed_text = ''
     for line in lines:
-        if re.match(r'define \S+ .*', line):
-            line = line.split(' ', 2)
+        if re.match(r'\s*define [a-zA-Z][a-zA-Z0-9_]* .*', line):
+            line = line.lstrip().split(' ', 2)
             definitions[line[1]] = line[2]
         else:
             processed_text += line+'\n'
 
     for definition in definitions:
-        for i in range(2):
-            processed_text = re.sub(_id_pattern.format(definition),
-                                    rf'\1{definitions[definition]}\3',
-                                    processed_text)
+        processed_text = re.sub(_id_pattern.format(definition),
+                                definitions[definition],
+                                processed_text)
 
     return processed_text
