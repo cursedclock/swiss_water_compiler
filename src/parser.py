14fb90ad077eab5564d44910dd7083815de70792
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2021-10-29 20:27:55 +0330

    really parser init, LOL!

--- src/parser.py
+++ src/parser.py
@@ -1,19 +1,23 @@
 import ply.yacc as yacc
 from lexer import tokens
 
-def p_start(p):
-    '''s : mac
-         | empty'''
+def p_Start(p):
+    '''S : Macro'''
     if p[1] != None:
         p[0] = 'done'
 
-def p_mac(p):
-    '''mac : mac macro
-           | macro'''
+def p_Macro(p):
+    '''Macro : macro
+            | empty'''
     p[0] = p[1]
 
 def p_macro(p):
-    '''macro : IMPORT STRINGLITERAL'''
+    '''macro : macro macroStmt
+           | macroStmt'''
+    p[0] = p[1]
+
+def p_macroStmt(p):
+    '''macroStmt : IMPORT STRINGLITERAL'''
     p[0] = p[1]
 
 def p_empty(p):
