05cc329d5eb8b76292dbc6e9e4d94e6baf3f3cfb
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2021-10-30 11:02:08 +0330

    done until FunctionDecl

--- src/parser.py
+++ src/parser.py
@@ -1,11 +1,15 @@
 import ply.yacc as yacc
 from lexer import tokens
 
-def p_Start(p):
-    '''S : Macro'''
-    if p[1] != None:
+
+# Program
+def p_Program(p):
+    '''Program : Macro Decl'''
+    if p[2] != None:
         p[0] = 'done'
 
+
+# Macro
 def p_Macro(p):
     '''Macro : macro
              | empty'''
@@ -17,9 +21,51 @@ def p_macro(p):
     p[0] = p[1]
 
 def p_macroStmt(p):
-    '''macroStmt : IMPORT STRINGLITERAL'''
+    '''macroStmt : IMPORT STRINGLITERAL
+                 | DEFINE ID defineStmt'''
     p[0] = p[1]
 
+# how to implement * ?
+def p_defineStmt(p):
+    '''defineStmt : ID'''
+    p[0] = p[1]
+
+
+# Decl
+def p_Decl(p):
+    '''Decl : Decl declStmt
+           | declStmt'''
+    p[0] = p[1]
+
+def p_declStmt(p):
+    '''declStmt : VariableDecl'''
+    p[0] = p[1]
+
+
+# VariableDecl
+def p_VariableDecl(p):
+    '''VariableDecl : Variable SEMICOLON'''
+    p[0] = p[1]
+
+
+# Variable
+def p_Variable(p):
+    '''Variable : Type ID'''
+    p[0] = p[1]
+
+
+# Type
+def p_Type(p):
+    '''Type : INT
+            | DOUBLE
+            | BOOL
+            | STRING
+            | ID
+            | Type OBRACKET CBRACKET'''
+    p[0] = p[1]
+
+
+# related to ply
 def p_empty(p):
     'empty :'
     pass
