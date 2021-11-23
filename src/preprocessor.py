0990291ba99c531da81fe95479d0d6e40da40a49
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2021-11-23 10:45:58 +0330

    fix bugs related to define to obey the grammar rules
    No more The Cool Commit :(

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -7,16 +7,24 @@ def pre_process(text: str) -> str:
     definitions = OrderedDict()
     lines = text.splitlines()
     processed_text = ''
+    obey_grammar_order = True
     for line in lines:
-        if re.match(r'\s*define [a-zA-Z][a-zA-Z0-9_]* .*', line):
-            line = line.lstrip().split(' ', 2)
-            definitions[line[1]] = line[2]
+        if re.match(r'\s*define.*', line):
+            match = re.match(r'(\s*define\s*)([a-zA-Z][a-zA-Z0-9_]*)( )([^\s].*)', line)
+            if match and obey_grammar_order:
+                definitions[match.group(2)] = match.group(4)
+            else:
+                raise DefineError
         else:
+            if not re.match(r'\s*import.*', line):
+                obey_grammar_order = False
             processed_text += line+'\n'
 
     for definition in definitions:
         processed_text = re.sub(_id_pattern.format(definition),
                                 definitions[definition],
                                 processed_text)
-
     return processed_text
+
+class DefineError(Exception):
+    pass
