2a66d8ab3cbc094d69051df38634ed8845b86e09
mostafa <mos1380n@gmail.com>
2022-02-22 09:13:53 +0330

    Fix lexer tests

--- test/lexer/main.py
+++ test/lexer/main.py
@@ -4,7 +4,7 @@ src_dir = os.path.dirname(os.path.dirname(current_dir))
 sys.path.append(src_dir)
 
 from src import pre_process
-from lexer import reserved, op_tokens, lexer
+from src.lexer import reserved, op_tokens, lexer
 
 import getopt
 def main(argv):
